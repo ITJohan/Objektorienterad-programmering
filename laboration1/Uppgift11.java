8107b15b4f56562f581a90c78b6777bbfbe05c49
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-09-24 15:40:22 +0200

    Update Uppgift11.java
    Handles cancel option, removed zero function

--- laboration1/Uppgift11.java
+++ laboration1/Uppgift11.java
@@ -4,33 +4,30 @@ public class Uppgift11 {
     public static void main(String[] arg) {
 
         // variable setup
-        String indata = null;
-        int date = 0, year = 0, day = 0, month = 0;
+        String indata = "", newDate = "";
+        int date, year, day, month;
 
         // do until cancel
-        while (true) {
+        while (indata != null) {
 
             // ask user for input
             indata = JOptionPane.showInputDialog("Ange datum på formen yymmdd: ");
-            date = Integer.parseInt(indata);
+            if (indata != null) {
+                date = Integer.parseInt(indata);
+            } else {
+                date = 0;
+            }
 
             // calculate new date format
             day = date % 100;
             month = (date % 10000 - day) / 100;
             year = (date - month * 100 - day) / 10000;
+            
+            // date formatting
+            newDate = String.format("%02d/%02d/%02d", month, day, year);
 
             // print the converted year
-            JOptionPane.showMessageDialog(null, zero(month) + "/" + zero(day) + "/" + zero(year));
-        }
-    }
-
-    // adds a zero in front of the input number if single digit
-    public static String zero(int i) {
-
-        if (i < 10) {
-            return "0" + i;
-        } else {
-            return "" + i;
+            JOptionPane.showMessageDialog(null, newDate);
         }
     }
 }
