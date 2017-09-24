cf6de4b09e6a5fe8dd0edb1d76fdae0c76f75c1c
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-09-24 15:11:16 +0200

    Update Uppgift08.java
    Handle cancel option

--- laboration1/Uppgift08.java
+++ laboration1/Uppgift08.java
@@ -2,12 +2,19 @@ import javax.swing.*;
 
 public class Uppgift08 {
     public static void main(String[] arg) {
+        // variable initialization
+        double vaxelkurs = 0, sek = 0;
 
         // ask for input
         String indata = JOptionPane.showInputDialog("Ange växelkursen mellan Euro och SEK: ");
-        double vaxelkurs = Double.parseDouble(indata);
+        if (indata != null) { // handle cancel option
+            vaxelkurs = Double.parseDouble(indata);
+        }
+        
         indata = JOptionPane.showInputDialog("Ange antal SEK: ");
-        double sek = Double.parseDouble(indata);
+        if (indata != null) { // handle cancel option
+            sek = Double.parseDouble(indata);
+        }
 
         // calculate exchange rate and print
         String euro = String.format("%.2f", sek * vaxelkurs);
