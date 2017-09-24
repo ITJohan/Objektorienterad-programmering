a726a27a282d5a5869227f15fb821f2b155138ee
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-09-24 15:14:27 +0200

    Update Uppgift09.java
    Handle cancel option

--- laboration1/Uppgift09.java
+++ laboration1/Uppgift09.java
@@ -1,19 +1,20 @@
-/**
- * Created by jolindk on 2017-09-05.
- */
 import javax.swing.*;
 
 public class Uppgift09 {
     public static void main(String[] arg) {
+        // variable initialization
+        double v = 0, alpha = 0, g = 9.82;
 
         // ask for input
         String indata = JOptionPane.showInputDialog("Ange utgångshastigheten i m/s: ");
-        double v = Double.parseDouble(indata);
-        indata = JOptionPane.showInputDialog("Ange kastvinkeln i grader: ");
-        double alpha = Double.parseDouble(indata);
+        if (indata != null) {
+            v = Double.parseDouble(indata);
+        }
 
-        // variable setup
-        double g = 9.82;
+        indata = JOptionPane.showInputDialog("Ange kastvinkeln i grader: ");
+        if (indata != null) {
+            alpha = Double.parseDouble(indata);
+        }
 
         // calculate variables
         String h = String.format("%.2f", (Math.pow(v, 2) * Math.pow(Math.sin(Math.toRadians(alpha)), 2)) / (2 * g));
