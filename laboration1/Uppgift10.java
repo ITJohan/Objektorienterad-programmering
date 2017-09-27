5e7593c6b412d86f16094c012b8686575254d664
AlfredL-A <32062650+AlfredL-A@users.noreply.github.com>
2017-09-27 17:06:33 +0200

    Update Uppgift10.java

--- laboration1/Uppgift10.java
+++ laboration1/Uppgift10.java
@@ -4,23 +4,24 @@ import javax.swing.*;
 public class Uppgift10 {
     public static void main(String[] arg) {
         // variable initialization
-        double d = 0, A = 0, L = 0, f = 0, val = 0;
+        double d = 0, A = 0, L = 0, f = 0;
 
         // ask for input
         String indata = JOptionPane.showInputDialog("Ange d, A, L och f i följd, separerade med mellanslag: ");
-        if (indata != null) { // handle null case
+        if (indata != null && !(indata.equals(""))) { // handle null case
             Scanner sc = new Scanner(indata).useDelimiter("\\s* \\s*");
             d = sc.nextDouble();
             A = sc.nextDouble();
             L = sc.nextDouble();
             f = sc.nextDouble();
             sc.close();
+
+
+            // calculate class of boat
+            double val = (2 * d + Math.sqrt(A) + L - f) / 2.37;
+
+            // print class of boat
+            JOptionPane.showMessageDialog(null, "Båtens klass: " + val);
         }
-
-        // calculate class of boat
-        double val = (2 * d + Math.sqrt(A) + L - f) / 2.37;
-
-        // print class of boat
-        JOptionPane.showMessageDialog(null, "Båtens klass: " + val);
     }
 }
