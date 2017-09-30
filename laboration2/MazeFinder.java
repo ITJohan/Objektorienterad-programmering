9fd5dddb837cc23bdd890a32df27159545d8a394
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-09-30 13:17:37 +0200

    Update MazeFinder.java

--- laboration2/MazeFinder.java
+++ laboration2/MazeFinder.java
@@ -1,5 +1,9 @@
 public class MazeFinder { 
-	private Robot robot; 
+	private Robot robot;
+
+	// booleans for checking walls, true if wall, false if no wall
+	private boolean frontSide, rightSide, backSide, leftSide;
+
 	public static void main(String[] args) { 
 		MazeFinder finder = new MazeFinder(); 
 		finder.createEnviroment(); 
@@ -8,7 +12,7 @@ public class MazeFinder {
 
 	public void createEnviroment() { 
 		RobotWorld world = RobotWorld.load("src/maze.txt"); 
-		robot = new Robot(2, 1, Robot.EAST, world);
+		robot = new Robot(2, 0, Robot.EAST, world);
 		robot.setDelay(50);
 	}//createEnviroment 
 
@@ -17,22 +21,65 @@ public class MazeFinder {
 	//        The robot is at the entrance of the maze.
 	//after:  The robot is at the exit of the maze
 	public void findExit() {
+		// initialize position variables
 		Location goalPos = new Location(7, 20);
 		Location currentPos = robot.getLocation();
 
+		// do until goal position
 		while (!currentPos.equals(goalPos)) {
-			if (robot.frontIsClear())
+
+			scanPaths();
+
+			// makes move according to wall setup
+			if ((frontSide && !rightSide && backSide && !leftSide) ||
+				(frontSide && !rightSide && !backSide && !leftSide) ||
+				(frontSide && rightSide && backSide && !leftSide)) {
+
 				robot.move();
-			else
+			} else if ((frontSide && rightSide && backSide && leftSide) ||
+					(!frontSide && rightSide && backSide && leftSide) ||
+					(!frontSide && !rightSide && backSide && leftSide) ||
+					(frontSide && !rightSide && backSide && leftSide)) {
+
 				robot.turnLeft();
-				if (!robot.frontIsClear())
-					robot.turnLeft();
-					if (!robot.frontIsClear()) {
-						robot.turnLeft();
-						robot.turnLeft();
-						if (!robot.frontIsClear())
-							robot.turnLeft();
-					}
+				robot.move();
+			} else if (!frontSide && !rightSide && backSide && !leftSide) {
+
+				uTurn();
+				robot.move();
+			} else if ((!frontSide && rightSide && backSide && !leftSide)) {
+
+				turnRight();
+				robot.move();
+			}
+
+			// update corrent position
+			currentPos = robot.getLocation();
 		}
-	}// findExit 
-}//MazeFinder
+	}// findExit
+
+	// scans all directions for walls
+	private void scanPaths() {
+		frontSide = robot.frontIsClear();
+		robot.turnLeft();
+		leftSide = robot.frontIsClear();
+		robot.turnLeft();
+		backSide = robot.frontIsClear();
+		robot.turnLeft();
+		rightSide = robot.frontIsClear();
+		robot.turnLeft();
+	} // scanPaths
+
+	// makes a right turn
+	private void turnRight() {
+		robot.turnLeft();
+		robot.turnLeft();
+		robot.turnLeft();
+	}
+
+	// makes a u-turn
+	private void uTurn() {
+		robot.turnLeft();
+		robot.turnLeft();
+	}
+}// MazeFinder
