f26b60a005297d43a23e301770417e573b5eacbb
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-09-30 13:33:07 +0200

    Update Escaper.java

--- laboration2/Escaper.java
+++ laboration2/Escaper.java
@@ -1,5 +1,9 @@
 public class Escaper { 
-	private Robot robot; 
+	private Robot robot;
+
+	// booleans for checking walls, true if wall, false if no wall
+	private boolean frontSide, rightSide, backSide, leftSide;
+
 	public static void main(String[] args) { 
 		Escaper escaper = new Escaper(); 
 		escaper.createEnviroment(); 
@@ -8,13 +12,66 @@ public class Escaper {
 
 	public void createEnviroment() { 
 		RobotWorld  world = RobotWorld.load("src/room.txt"); 
-		robot = new Robot(3, 3, Robot.WEST, world); 
-		robot.setDelay(250); 
+		robot = new Robot(4, 5, Robot.WEST, world);
+		robot.setDelay(30);
 	}//createEnviroment 
  
 	//before: robot is inside the room
 	//after:  robot is in the cell representing the the dorrjamb
-	public void moveToEntrance() { 
-		//The implementation is your work to do!
-	}// moveToEntrance 
+	public void moveToEntrance() {
+		// initialize position variables
+		Location goalPos = new Location(4, 6);
+		Location currentPos = robot.getLocation();
+
+		// move forward until wall
+		while (robot.frontIsClear())
+			robot.move();
+
+		// then follow wall principle
+		while (!currentPos.equals(goalPos)) {
+
+			scanPaths();
+
+			// makes move according to wall setup
+			if ((frontSide && !rightSide && backSide && !leftSide) ||
+					(frontSide && !rightSide && !backSide && !leftSide) ||
+					(frontSide && rightSide && backSide && !leftSide)) {
+
+				robot.move();
+			} else if ((!frontSide && !rightSide && backSide && leftSide) ||
+					(frontSide && !rightSide && backSide && leftSide) ||
+					(frontSide && rightSide && backSide && leftSide)) {
+
+				robot.turnLeft();
+				robot.move();
+			} else if ((!frontSide && rightSide && backSide && !leftSide) ||
+					(!frontSide && rightSide && backSide && leftSide)) {
+
+				turnRight();
+				robot.move();
+			}
+
+			// update corrent position
+			currentPos = robot.getLocation();
+		}
+	}// moveToEntrance
+
+	// scans all directions for walls
+	private void scanPaths() {
+		frontSide = robot.frontIsClear();
+		robot.turnLeft();
+		leftSide = robot.frontIsClear();
+		robot.turnLeft();
+		backSide = robot.frontIsClear();
+		robot.turnLeft();
+		rightSide = robot.frontIsClear();
+		robot.turnLeft();
+	} // scanPaths
+
+	// makes a right turn
+	private void turnRight() {
+		robot.turnLeft();
+		robot.turnLeft();
+		robot.turnLeft();
+	}
 }//Escaper
